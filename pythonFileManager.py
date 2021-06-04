96fe34bc8bc324fb2f9be437ce3dd6bb78928e7c
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2021-06-04 07:21:58 +0100

    Modified pythonFileManager.py

--- pythonFileManager.py
+++ pythonFileManager.py
@@ -1,42 +1,3 @@
-def main():
-    i=1
-    value="0"
-    while i:
-        print("Selection Menu:")
-        print("0. Exit Program")
-        print("1. Read from a file.")
-        print("2. Write integers to a file.")
-        print("3. Append integers to a file.")
-        answer=input("What would you like to do? ")
-        if answer=="0":
-            break
-        elif answer=="1":
-            readFile=input("Please enter a file name: ")
-            in_file=checkFile(readFile)
-            line=in_file.readline()
-            while line !='':
-                print(line)
-                line=in_file.readline()
-            in_file.close
-        elif answer=="2":
-            writeFile=input("Enter a file you'd like to write to: ")
-            in_file=open(writeFile,'w')
-            value=input("Enter in a number or done to finish: ")
-            while (value != 'done'):
-                value = checkInt(value)
-                if value!=False:
-                    in_file.write(str(value)+"\n")
-                value=input("Enter in a number or done to finish: ")
-            in_file.close()
-        elif answer=="3":
-            writeFile=input("Enter a file you'd like to amend to: ")
-            in_file=open(writeFile,'a')
-            while (value != 'done'):
-                value=(input("Enter in a number: "))
-                in_file.write(str(checkInt(value)))
-                value=input("Type done if done, if not enter any key to continue: ")
-            in_file.close()
-
 def checkFile(file_name):
     try:
         in_file=open(file_name,"r")
@@ -52,7 +13,53 @@ def checkInt(value):
         return valid_int
     except:
         return False
-        
-    
 
+def main():
+    i=1
+    value="0"
+    while i:
+        print("Selection Menu:")
+        print("0. Exit Program")
+        print("1. Read from a file.")
+        print("2. Write integers to a file.")
+        print("3. Append integers to a file.")
+        answer=input("What would you like to do? ")
+        if answer=="0":
+            exit()
+            
+        elif answer=="1":
+            readFile=input("Please enter a file name: ")
+            in_file=checkFile(readFile)
+            if in_file is False: # checks if the file exists 
+                print("")
+                main()
+            line=in_file.readline()
+            while line !='':
+                print(line)
+            line=in_file.readline()
+            in_file.close
+
+            
+        elif answer=="2":
+            try:
+                writeFile=input("Enter a file you'd like to write to: ")
+                in_file=open(writeFile,'w')
+                value=input("Enter in a number or done to finish: ")
+                while (value != 'done'):
+                    value = checkInt(value)
+                    if value!=False:
+                        in_file.write(str(value)+"\n")
+                    value=input("Enter in a number or done to finish: ")
+                in_file.close()
+            except:
+                print("[X] An error occurred ")
+            
+        elif answer=="3":
+            writeFile=input("Enter a file you'd like to amend to: ")
+            in_file=open(writeFile,'a')
+            while (value != 'done'):
+                value=(input("Enter in a number: "))
+                in_file.write(str(checkInt(value)))
+                value=input("Type done if done, if not enter any key to continue: ")
+            in_file.close()
 main()
